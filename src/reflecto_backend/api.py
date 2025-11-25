058df0d1580c517ab8cb679b20f4adfbf64261fc
isaac <isaacyong@naver.com>
2025-11-26 02:38:06 +0900

    add wavelen in ai_guess

--- src/reflecto_backend/api.py
+++ src/reflecto_backend/api.py
@@ -9,10 +9,10 @@ from .core.inference import XRRInferenceEngine
 
 WEIGHTS_DIR = Path(r"data/models")
 
-def ai_guess(tths: np.ndarray, refl: np.ndarray) -> tuple[list[ParamSet], ParamSet]:
+def ai_guess(tths: np.ndarray, refl: np.ndarray, wavelen:float = 1.54) -> tuple[list[ParamSet], ParamSet]:
 
     inference_engine: XRRInferenceEngine = XRRInferenceEngine(exp_dir=WEIGHTS_DIR / "251126_6out")
-    qs: np.ndarray = tth2q(tths)
+    qs: np.ndarray = tth2q(tths, wavelen)
     preds: np.ndarray = inference_engine.predict(qs, refl)
     pred_f_d, pred_f_sig, pred_f_sld = preds[0], preds[1], preds[2]
     pred_s_d, pred_s_sig, pred_s_sld = preds[3], preds[4], preds[5]
